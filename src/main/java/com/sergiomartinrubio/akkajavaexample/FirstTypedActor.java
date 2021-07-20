8c4b3b0bd307678e624b9ea4bb922bdd8b18dcbe
Sergio Martin Rubio <econsergio@gmail.com>
2021-07-20 09:16:31 +0200

    Add signals to typed actor

--- src/main/java/com/sergiomartinrubio/akkajavaexample/FirstTypedActor.java
+++ src/main/java/com/sergiomartinrubio/akkajavaexample/FirstTypedActor.java
@@ -2,6 +2,10 @@ package com.sergiomartinrubio.akkajavaexample;
 
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.PreRestart;
+import akka.actor.typed.SpawnProtocol;
+import akka.actor.typed.Terminated;
 import akka.actor.typed.javadsl.AbstractBehavior;
 import akka.actor.typed.javadsl.ActorContext;
 import akka.actor.typed.javadsl.Behaviors;
@@ -15,7 +19,7 @@ public class FirstTypedActor extends AbstractBehavior<FirstTypedActor.FooMessage
 
     private final ActorRef<ChildTypedActor.BarMessage> childTypedActor;
 
-    public static record FooMessage(String message) {
+    public static record FooMessage(String message) implements SpawnProtocol.Command {
 
     }
 
@@ -35,6 +39,9 @@ public class FirstTypedActor extends AbstractBehavior<FirstTypedActor.FooMessage
     public Receive<FooMessage> createReceive() {
         return newReceiveBuilder()
                 .onMessage(FooMessage.class, this::onFooMessage)
+                .onSignal(PreRestart.class, this::onPreRestart)
+                .onSignal(Terminated.class, this::onTerminated)
+                .onSignal(PostStop.class, this::onPostStop)
                 .build();
     }
 
@@ -42,6 +49,21 @@ public class FirstTypedActor extends AbstractBehavior<FirstTypedActor.FooMessage
         System.out.println("Received Foo message: " + message.message);
         childTypedActor.tell(new ChildTypedActor.BarMessage("hello from top level actor"));
         return this; // a "Behavior" type is expected to be returned
+    }
+
+    private FirstTypedActor onPreRestart(PreRestart preRestart) {
+        System.out.println("Job is about to restart.");
+        return this;
+    }
+
+    private FirstTypedActor onTerminated(Terminated terminated) {
+        System.out.println("Job" + terminated.getRef().path().name() + "stopped.");
+        return this;
+    }
+
+    private FirstTypedActor onPostStop(PostStop postStop) {
+        System.out.println("Job is stopped.");
+        return this;
     }
 
 }
--- src/main/java/com/sergiomartinrubio/akkajavaexample/TypedMain.java
+++ src/main/java/com/sergiomartinrubio/akkajavaexample/TypedMain.java
@@ -8,5 +8,6 @@ public class TypedMain {
         // Create top level actor for the actor system "typed-actor-system"
         ActorSystem<FirstTypedActor.FooMessage> system = ActorSystem.create(FirstTypedActor.create(), "typed-actor-system");
         system.tell(new FirstTypedActor.FooMessage("hello world"));
+        system.terminate();
     }
 }
